40851936e6ca7ad3460c36932f8bf7bb96722ac0
thomazllr <guithomaz270@gmail.com>
2025-02-04 09:00:16 -0300

    refactoring validator

--- src/main/java/com/thomazllr/url_shortener/service/validator/UrlValidator.java
+++ src/main/java/com/thomazllr/url_shortener/service/validator/UrlValidator.java
@@ -8,16 +8,11 @@ public class UrlValidator {
 
     public Source getUrlSource(String url) {
         String lowerCaseUrl = url.toLowerCase();
-        if (lowerCaseUrl.contains("youtube.com") || lowerCaseUrl.contains("youtu.be")) {
-            return Source.YOUTUBE;
-        } else if (lowerCaseUrl.contains("twitter.com") || lowerCaseUrl.contains("x.com")) {
-            return Source.TWITTER;
-        } else if (lowerCaseUrl.contains("linkedin.com")) {
-            return Source.LINKEDIN;
-        } else if (lowerCaseUrl.contains("facebook.com") || lowerCaseUrl.contains("fb.com")) {
-            return Source.FACEBOOK;
-        } else {
-            return Source.OTHER;
-        }
+        if (lowerCaseUrl.contains("youtube.com") || lowerCaseUrl.contains("youtu.be")) return Source.YOUTUBE;
+        if (lowerCaseUrl.contains("twitter.com") || lowerCaseUrl.contains("x.com")) return Source.TWITTER;
+        if (lowerCaseUrl.contains("linkedin.com")) return Source.LINKEDIN;
+        if (lowerCaseUrl.contains("facebook.com") || lowerCaseUrl.contains("fb.com")) return Source.FACEBOOK;
+        return Source.OTHER;
     }
+
 }
